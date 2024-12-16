dca976b6655ae4a698c8e850580a9af25bb47193
Jorge Leonardo Cardenas <leonardo.cardenas@rappi.com>
2024-12-15 22:35:34 -0500

    items montados

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import NavBar from './components/NavBar/NavBar'
 
 function App() {
   return (
     <>
       <NavBar />
+      <ItemListContainer />
+      <ItemDetailContainer />
     </>
   )
 }
